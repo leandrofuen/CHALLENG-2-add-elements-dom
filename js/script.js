2f809f630b840cf700454f4c8382b165aba311f0
leandrofuen <leandro_fuba@hotmail.es>
2025-11-05 14:07:37 +0000

    ya estamos trabajando en el ejercicio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+
+
+/* Este ejercicio consiste en crear una página web donde los usuarios puedan agregar elementos a una lista utilizando un cuadro de diálogo `prompt`. Luego, los elementos ingresados se mostrarán en una lista en la página.
+
+Al pulsar el botón 'agregar elemento' aparecerá el cuadro de dialogo (`prompt`) y ahí se introducirá el elemento que después aparecerá en la lista.
+
+La estructura ya está creada en el HTML, así solo tendrás que trabajar con JavaScript. */
+
+
+var elemento = prompt("Introduce un elemento")
+
